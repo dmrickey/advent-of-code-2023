aa7163902190aa902ce7ae7a87c701d40a541171
dmrickey <dmrickey@gmail.com>
2023-12-15 10:03:00 -0600

    day 4 part 2

--- day-04/day-04.mjs
+++ day-04/day-04.mjs
@@ -12,19 +12,22 @@ export default () => {
         constructor(str) {
             const [first, second] = str.split(':');
             this.id = +first.split(' ')[1];
-            const [sWinngingNumbers, sNumbers] = second.split('|');
+            const [sWinningNumbers, sNumbers] = second.split('|');
             this.numbers = sNumbers.split(' ').filter(truthiness).map(x => +x.trim());
-            this.winngingNumbers = sWinngingNumbers.split(' ').filter(truthiness).map(x => +x.trim());
+            this.winningNumbers = sWinningNumbers.split(' ').filter(truthiness).map(x => +x.trim());
         }
 
-        _points;
+        /** @returns {number} */
         get points() {
-            return this._points ??= (() => {
-                const values = intersection(this.numbers, this.winngingNumbers);
-                return values.length
-                    ? Math.pow(2, values.length - 1)
-                    : 0;
-            })();
+            return this.matches
+                ? Math.pow(2, this.matches - 1)
+                : 0;
+        }
+
+        /** @returns {number} */
+        get matches() {
+            const values = intersection(this.numbers, this.winningNumbers);
+            return values.length;
         }
     }
 
@@ -44,10 +47,23 @@ export default () => {
     // answer = 25004
 
     const part2 = () => {
+        const cards = contents.map(x => new Card(x));
+        const actualPoints = [...new Array(cards.length)];
 
-        console.log('day 4 part 1:', answer);
+        for (let i = cards.length - 1; i >= 0; i--) {
+            const card = cards[i];
+            const matches = card.matches;
+            let cardQty = 1;
+            for (let j = 1; j <= matches; j++) {
+                cardQty += actualPoints[i + j] || 0;
+            }
+            actualPoints[i] = cardQty;
+        }
+
+        const answer = actualPoints.sum();
+        console.log('day 4 part 2:', answer);
     }
-    // part2();
+    part2();
     // test output - 30
-    // answer - 
+    // answer - 14427616
 };
